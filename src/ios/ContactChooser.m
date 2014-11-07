2002141764f8d05358a38de70564ddbb48cd8f39
Oliver Degener <o.degener@monday-consulting.com>
2014-11-07 15:07:36 +0100

    iOS: return selected value and first value for other properties

--- src/ios/ContactChooser.m
+++ src/ios/ContactChooser.m
@@ -26,31 +26,46 @@
                                 property:(ABPropertyID)property
                               identifier:(ABMultiValueIdentifier)identifier
 {
+    NSString *email = @"";
+    ABMultiValueRef multiEmails = ABRecordCopyValue(person, kABPersonEmailProperty);
     if (kABPersonEmailProperty == property)
     {
-        ABMultiValueRef multi = ABRecordCopyValue(person, kABPersonEmailProperty);
-        int index = ABMultiValueGetIndexForIdentifier(multi, identifier);
-        NSString *email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multi, index);
-        NSString *displayName = (__bridge NSString *)ABRecordCopyCompositeName(person);
-        ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
-        NSString* phoneNumber = @"";
-        for(CFIndex i = 0; i < ABMultiValueGetCount(multiPhones); i++) {
-            if(identifier == ABMultiValueGetIdentifierAtIndex (multiPhones, i)) {
-                phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, i);
-                break;
-            }
-        }
-
-        NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
-        [contact setObject:email forKey: @"email"];
-        [contact setObject:displayName forKey: @"displayName"];
-        [contact setObject:phoneNumber forKey: @"phoneNumber"];
-
-        [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact] toSuccessCallbackString:self.callbackID]];
-        [self.viewController dismissViewControllerAnimated:YES completion:nil];
-        return NO;
+        // user selected an email property -> retrieve selected email
+        int index = ABMultiValueGetIndexForIdentifier(multiEmails, identifier);
+        email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiEmails, index);
     }
-    return YES;
+    else if (ABMultiValueGetCount(multiEmails) > 0)
+    {
+        // user did not select an email property -> default to the contact's first email (if available)
+        email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiEmails, 0);
+    }
+
+    // retrieve the contact's composite name
+    NSString *displayName = (__bridge NSString *)ABRecordCopyCompositeName(person);
+
+    NSString* phoneNumber = @"";
+    ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    if (kABPersonPhoneProperty == property)
+    {
+        // user selected a phone property -> retrieve selected phone number
+        int index = ABMultiValueGetIndexForIdentifier(multiPhones, identifier);
+        phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, index);
+    }
+    else if (ABMultiValueGetCount(multiPhones) > 0)
+    {
+        // user did not select a phone property -> default to the contact's first number (if available)
+        phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, 0);
+    }
+
+    // stick everything together
+    NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
+    [contact setObject:email forKey: @"email"];
+    [contact setObject:displayName forKey: @"displayName"];
+    [contact setObject:phoneNumber forKey: @"phoneNumber"];
+
+    [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact]toSuccessCallbackString:self.callbackID]];
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+    return NO;
 }
 
 - (BOOL) personViewController:(ABPersonViewController*)personView shouldPerformDefaultActionForPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifierForValue
